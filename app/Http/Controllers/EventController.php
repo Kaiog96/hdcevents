fd51c2cb2a49835ba8784134bd078dd465311bbf
kaiogarcia <kaiogarcia@employer.com.br>
2024-11-19 17:11:31 -0300

    Adicioanr search

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -8,11 +8,24 @@ use App\Models\Event;
 
 class EventController extends Controller
 {
+
     public function index()
     {
-        $events = Event::all();
 
-        return view("welcome", ["events" => $events]);
+        $search = request('search');
+
+        if ($search) {
+
+            $events = Event::where([
+                ['title', 'like', '%' . $search . '%']
+            ])->get();
+
+        } else {
+            $events = Event::all();
+        }
+
+        return view('welcome', ['events' => $events, 'search' => $search]);
+
     }
 
     public function create()
@@ -26,11 +39,13 @@ class EventController extends Controller
         $event = new Event;
 
         $event->title = $request->title;
+        $event->date = $request->date;
         $event->city = $request->city;
         $event->private = $request->private;
         $event->description = $request->description;
         $event->items = $request->items;
 
+        // Image Upload
         if ($request->hasFile('image') && $request->file('image')->isValid()) {
 
             $requestImage = $request->image;
@@ -48,7 +63,6 @@ class EventController extends Controller
         $event->save();
 
         return redirect('/')->with('msg', 'Evento criado com sucesso!');
-        ;
 
     }
 
@@ -60,4 +74,5 @@ class EventController extends Controller
         return view('events.show', ['event' => $event]);
 
     }
+
 }
