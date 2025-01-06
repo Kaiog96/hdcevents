1bd58233c9b8c2ab8bb1a212d78282e4c9a188cd
kaiogarcia <kaiogarcia@employer.com.br>
2025-01-06 09:30:20 -0300

    Commit novo

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -82,7 +82,7 @@
                     flashMessage.style.transition = "opacity 0.5s ease";
                     flashMessage.style.opacity = "0";
                     setTimeout(() => flashMessage.remove(), 500);
-                }, 3000);
+                }, 2500);
             }
         });
     </script>
